38192783826e175832497cd1fa0df08f2fe08c73
STUDENTDaneJones <STUDENTDaneJones@TECH11>
2023-10-27 08:44:06 -0700

    Add project files.

---
+++ Justus/Program.cs
@@ -0,0 +1,34 @@
+﻿
+    class Program
+{ 
+    static void Main(string[] args)
+{
+    Random r = new Random();
+    int num = r.Next(1, 101);
+    bool win = false;
+
+    do
+    {
+        Console.WriteLine("Guess a number between 1 - 100");
+        string s = Console.ReadLine();
+
+        int i = int.Parse(s);
+
+        if (i > num)
+        {
+            Console.WriteLine("Your number is to high!");
+        }
+
+        else if (i < num)
+        {
+            Console.WriteLine("Your number is to low!");
+        }
+
+        else if (i == num)
+        {
+            Console.WriteLine("You got it!");
+            win = true;
+        }
+    } while (win == false);
+}
+}
